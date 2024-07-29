774405fdb5b944a85dda6e1a7bd2095b0ba29a2d
HudaAbdalla <hudamaahmed@gmail.com>
2024-07-29 14:37:03 +0300

    [Test]app_one1

---
+++ custom_addons/app_one/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "App One",
+
+    'author': "huda ",
+   
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
